928ed6b4d568ab732d5832f30fd9ab8f53663b64
alen <alen@localhost.localdomain>
2018-04-24 16:55:15 +0200

    Unify db.dump

--- fakedata/dumpdb.sh
+++ fakedata/dumpdb.sh
@@ -4,4 +4,4 @@
 # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # sudo apt-get update
 # sudo apt-get install postgresql-10
-pg_dump -cC --format=c -U postgres postgres -h 192.168.90.90 > db.sql
+pg_dump -cC --format=c -U postgres postgres -h 192.168.90.90 > db.dump
